38fb41d8b2665c6fe0a7e60b2278903728b5ac09
Ayush Kumar <2k23.csaiml2311319@gmail.com>
2026-06-13 20:08:43 +0530

    Configure REST API routes

---
+++ backend/api/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from .views import (
+    scam_detect_view,
+    profile,
+    leaderboard,
+    register
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('detect-scam/', scam_detect_view, name='detect-scam'),
+    path("register/", register),
+    path("login/", TokenObtainPairView.as_view()),
+    path("refresh/", TokenRefreshView.as_view()),
+    path("profile/", profile),
+    path("leaderboard/", leaderboard)
+    # More endpoints will be added later
+]
